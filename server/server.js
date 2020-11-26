11c1c097df352e3f14f393575d04f63fc70a505f
Derek <derekwhite.m@gmail.com>
2020-11-26 10:02:57 -0500

    maybe ready to deploy, testing

--- server/server.js
+++ server/server.js
@@ -6,14 +6,15 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
@@ -34,8 +35,14 @@ app.use('/portfolio', portfolioRouter);
 app.use('/principal', principalRouter);
 app.use('/account', accountRouter);
 
+app.use(express.static(path.join(__dirname, '../client/build')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build'))
+})
+
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
+    console.log(`Server is running on port: ${port}`)
+})
 
 //getStocks()
