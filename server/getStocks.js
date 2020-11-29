6fa245cc646f77776dc4f796e780fb20d867aa13
Derek White <derekwhite.m@gmail.com>
2020-11-29 11:31:02 -0500

    setup wakeUpDyno and getStocks interval

--- server/getStocks.js
+++ server/getStocks.js
@@ -3,15 +3,13 @@
 let Asset = require('./models/asset.model')
 const fetch = require('node-fetch')
 
-
-const getStocks = () => {
+const getPrices = () => {
     Asset.find({})
     .then( assets => { 
         for ( let i = 0; i < assets.length; i++ ) { 
             let ticker = assets[i].ticker
             let url = `https://yahoo-finance-low-latency.p.rapidapi.com/v8/finance/spark?symbols=${ticker}&interval=1mo&range=5y`
-            fetch(url,{
-                method: 'get',
+            fetch(url, {
                 headers: {
                     'x-rapidapi-key': '167deac947mshe187dd44a1f25dfp1786eejsndd55ff6025e5',
                     'x-rapidapi-host': 'yahoo-finance-low-latency.p.rapidapi.com'
@@ -29,6 +27,13 @@ const getStocks = () => {
             })
         } 
     })
+}
+
+const getStocks = () => {
+    let numDays = 30
+    let interval = numDays * 86400000
+    getPrices()
+    setInterval(getPrices, interval)
 }
 
 module.exports = getStocks
--- server/server.js
+++ server/server.js
@@ -1,39 +1,41 @@
 require('dotenv').config()
 
-const getPrices = require('./getStocks')
+const getStocks = require('./getStocks')
+const wakeUpDyno = require('./wakeUpDyno')
 
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const path = require('path')
 
-const app = express();
+const app = express()
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
+const DYNO_URL = 'https://efficient-portfolio.herokuapp.com/'
 
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
-const connection = mongoose.connection;
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+const connection = mongoose.connection
 connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-});
+    console.log("MongoDB database connection established successfully")
+})
 
-const assetsRouter = require('./routes/assets');
+const assetsRouter = require('./routes/assets')
 const incomeRouter = require('./routes/income')
-const portfolioRouter = require('./routes/portfolio');
-const principalRouter = require('./routes/principal');
-const accountRouter = require('./routes/account');
+const portfolioRouter = require('./routes/portfolio')
+const principalRouter = require('./routes/principal')
+const accountRouter = require('./routes/account')
 
-app.use('/assets', assetsRouter);
-app.use('/income', incomeRouter);
-app.use('/portfolio', portfolioRouter);
-app.use('/principal', principalRouter);
-app.use('/account', accountRouter);
+app.use('/assets', assetsRouter)
+app.use('/income', incomeRouter)
+app.use('/portfolio', portfolioRouter)
+app.use('/principal', principalRouter)
+app.use('/account', accountRouter)
 
 app.use(express.static(path.join(__dirname, '../client/build')))
 
@@ -42,7 +44,7 @@ app.get('*', (req, res) => {
 })
 
 app.listen(port, () => {
+    getStocks()
+    wakeUpDyno(DYNO_URL)
     console.log(`Server is running on port: ${port}`)
 })
-
-//getStocks()
