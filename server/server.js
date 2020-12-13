4099a4feeb128f7af420cc1e2704a72fc7414d52
Derek White <derekwhite.m@gmail.com>
2020-12-12 20:44:44 -0500

    commit

--- server/server.js
+++ server/server.js
@@ -2,38 +2,32 @@ require('dotenv').config()
 
 const getStocks = require('./getStocks')
 const wakeUpDyno = require('./wakeUpDyno')
-
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const path = require('path')
-
 const app = express()
-
 const port = process.env.PORT || 5000
 const DYNO_URL = 'https://efficient-portfolio.herokuapp.com/'
-
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
 const uri = process.env.ATLAS_URI
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
 const connection = mongoose.connection
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully")
-})
-
 const assetsRouter = require('./routes/assets')
 const portfolioRouter = require('./routes/portfolio')
 const accountRouter = require('./routes/account')
 
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/assets', assetsRouter)
 app.use('/portfolio', portfolioRouter)
 app.use('/account', accountRouter)
-
 app.use(express.static(path.join(__dirname, '../client/build')))
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully")
+})
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build'))
